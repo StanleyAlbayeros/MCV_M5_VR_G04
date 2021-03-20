1f4b4f67059eeb45cfe39bc181dea745d076a7dd
victor-prt <group04@uab.es>
2021-03-20 22:40:14 +0100

    task d

--- W3/task_d_retina.py
+++ W3/task_d_retina.py
@@ -84,31 +84,50 @@ if LOCAL_RUN:
     BASE_PATH = "../resources/KITTI-MOTS/"
 
 
-cfg = get_cfg()
-# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
-predictor = DefaultPredictor(cfg)
-
 for d in ["train"]:
     DatasetCatalog.register("kitti-mots_"+d, lambda d=d: get_dicts(BASE_PATH))
     MetadataCatalog.get("kitti-mots_train").set(thing_classes=["Pedestrian","Car"])
+
 kitti_mots_metadata = MetadataCatalog.get("kitti-mots_train")
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_50_FPN_1x.yaml"))
+cfg.DATASETS.TRAIN = ("kitti-mots_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/retinanet_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 16
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=True)
+trainer.train()
+
+
+# Inference should use the config with parameters that are used in training
+# cfg now already contains everything we've set previously. We changed it a little bit for inference:
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
 
 dataset_dicts = get_dicts(BASE_PATH)
 
 for rand in random.sample(dataset_dicts, 1):
     img = cv2.imread(rand["file_name"])
     visualizer = Visualizer(img[:, :, ::-1], metadata=kitti_mots_metadata, scale=0.5)
+    print(rand)
     out = visualizer.draw_dataset_dict(rand)
     plt.imshow(out.get_image()[:, :, ::-1])
     plt.show()
     cv2.imwrite("out_kittimotts_faster_rcnn.png", out.get_image()[:, :, ::-1])
 
-
 evaluator = COCOEvaluator("kitti-mots_train", cfg, False, output_dir="./output/")
 val_loader = build_detection_test_loader(cfg, "kitti-mots_train")
 print(inference_on_dataset(trainer.model, val_loader, evaluator))
-
