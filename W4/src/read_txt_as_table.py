4c349749bc64c3067060b5cecb529034ca896641
oriolcatalanyarza <oriolcatalan1994@gmail.com>
2021-04-12 02:20:57 +0200

    new readtxt as table

--- W4/src/read_txt_as_table.py
+++ W4/src/read_txt_as_table.py
@@ -1,39 +1,36 @@
 import os
 import re
-from ast import literal_eval
 from collections import OrderedDict
 
 import numpy as np
 from numpy import nan
-import pandas as pd
 import csv
-import json
 
 # This string is slightly different from your sample which had an extra bracket
-RESULTS_PATH="../task_a_txt/"
+RESULTS_PATH="../txt_results/"
 def read_file(file):
     f = open(os.path.join(RESULTS_PATH, file), "r")
     line = f.readline()
+    line2 = f.readline()
+
     match = re.search(r'^OrderedDict\((.+)\)\s*$', line)
     data = match.group(1)
+
+    match = re.search(r'Time elapsed: (.+)\s*$', line2)
+    et = match.group(1)
     # This allows safe evaluation: data can only be a basic data structure
-    return OrderedDict(eval(data))
+    return OrderedDict(eval(data)), et
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Convert txts to tables")
-    parser.add_argument('-path', type=dir_path)
+onlyfiles = [f for f in os.listdir(RESULTS_PATH) if os.path.isfile(os.path.join(RESULTS_PATH, f))]
+col = ["", "AP", "AP50", "AP75", "APs", "APm", "APl", "AP-Person", "AP-Other", "AP-Car", "Elapsed Time", "ratio AP:Time", "ratio AP50:Time","ratio Car:Time","ratio Person:Time"]
 
-fp=open(os.path.join(RESULTS_PATH, "bbox.csv"), "w")
+fp = open(os.path.join(RESULTS_PATH, "task_a_results.csv"), "w")
 writer = csv.writer(fp, delimiter='\t')
-writer.writerow(col)
-
-fp1=open(os.path.join(RESULTS_PATH, "segm.csv"), "w")
-writer1 = csv.writer(fp1, delimiter='\t')
-writer1.writerow(col)
-
+bboxes=[]
+segments=[]
 for file in onlyfiles:
-    content = read_file(file)
+    content, et = read_file(file)
     auxb=[]
     auxs=[]
     for i in list(content["bbox"].values()):
@@ -41,5 +38,33 @@ for file in onlyfiles:
     for i in list(content["segm"].values()):
         auxs.append(str(i).replace(".",","))
 
-    writer.writerow([file.replace(".txt", "")]+list(auxb))
-    writer1.writerow([file.replace(".txt", "")]+list(auxs))
+    times=[]
+    times.append(str(et).replace(".",","))
+    times.append(str(np.float_(et)/np.float_(content["bbox"]["AP"])).replace(".",","))
+    times.append(str(np.float_(et)/np.float_(content["bbox"]["AP50"])).replace(".",","))
+    times.append(str(np.float_(et)/np.float_(content["bbox"]["AP-Person"])).replace(".",","))
+    times.append(str(np.float_(et)/np.float_(content["bbox"]["AP-Car"])).replace(".",","))
+
+    bboxes.append([file.replace(".txt", "")]+list(auxb)+list(times))
+
+    times = []
+    times.append(str(et).replace(".", ","))
+    times.append(str(np.float_(et) / np.float_(content["segm"]["AP"])).replace(".", ","))
+    times.append(str(np.float_(et) / np.float_(content["segm"]["AP50"])).replace(".", ","))
+    times.append(str(np.float_(et) / np.float_(content["segm"]["AP-Person"])).replace(".", ","))
+    times.append(str(np.float_(et) / np.float_(content["segm"]["AP-Car"])).replace(".", ","))
+
+    segments.append([file.replace(".txt", "")]+list(auxs)+list(times))
+
+writer.writerow(["Bounding Boxes"])
+writer.writerow(col)
+for row in bboxes:
+    writer.writerow(row)
+
+writer.writerow([])
+
+writer.writerow(["Segmentation"])
+writer.writerow(col)
+
+for row in segments:
+    writer.writerow(row)
